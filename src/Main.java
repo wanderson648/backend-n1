21af80be84e64f4b4db228c613e1f82576f1511b
Wanderson Oliveira da Silva <wanderson.oliveira@senaicni.com.br>
2026-01-30 19:14:00 -0300

    primeiraclass

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
 
+                /*int anoDeLancamento = 2022;
+                System.out.println("Ano de lançamento:" + anoDeLancamento");
+                boolean incluidoNoPlano = true;
+                double notaDoFilme = 8.1;
+
+        double media = (9.8 + 6.3 + 8) / 3;*/
+        //Média calculada pelas 3 notas da Jack, Paulo e Suelem
     }
 }
--- src/PrimeiraClass.java
+++ src/PrimeiraClass.java
@@ -1,5 +1,9 @@
 public class PrimeiraClass {
     public static void main(String[] args) {
-        System.out.println("Concluí a aula 01 e agora estou mergulhando em Java!");
+        String saudacao = "Olá, meu nome é ";
+        String nome = "Alice ";
+        String continuacao = "e minha idade é ";
+        int idade = 17;
+        System.out.println(saudacao + nome + continuacao + idade);
     }
 }
