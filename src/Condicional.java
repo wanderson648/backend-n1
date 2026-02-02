9286a3943a4e4554c9458200e8bba81cd1901451
wanderson648 <testepedroca@gmail.com>
2026-02-02 08:51:15 -0300

    condicional

---
+++ src/Condicional.java
@@ -0,0 +1,16 @@
+public class Condicional {
+    public static void main(String[] args) {
+        int anoDeLancamento = 1990;
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+        String tipoPlano = "plus";
+
+// código omitido
+
+        if (incluidoNoPlano == true && tipoPlano.equals("plus")){
+            System.out.println("Filme liberado");
+        } else {
+            System.out.println("Deve pagar a locação");
+        }
+    }
+}
