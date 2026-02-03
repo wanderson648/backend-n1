68152533810c8eb22e04ebe7808a87d603c21847
Wanderson Oliveira da Silva <wanderson.oliveira@senaicni.com.br>
2026-02-03 18:46:39 -0300

    herança e poliformismo

--- src/modelos/Filme.java
+++ src/modelos/Filme.java
@@ -1,81 +1,13 @@
 package modelos;
 
-public class Filme {
-    String nome;
-    int anoDeLancamento;
-    boolean incluidoNoPlano;
-    double avaliacao;
-    private int totalDeAvaliacoes;
-    private double somaDasAvaliacoes;
-    int duracaoEmMinutos;
+public class Filme extends Titulo {
+    private String diretor;
 
-    public void exibeFichaTecnica(){
-        System.out.println("Nome do filme:" + nome);
-        System.out.println("Ano de lançamento:" + anoDeLancamento);
+    public String getDiretor() {
+        return diretor;
     }
 
-    public void avalia(double nota) {
-        somaDasAvaliacoes += nota;
-        totalDeAvaliacoes++;
-    }
-
-    public double pegaMedia(){
-        return somaDasAvaliacoes / totalDeAvaliacoes;
-    }
-
-    public int getTotalDeAvaliacoes() {
-        return totalDeAvaliacoes;
-    }
-
-    public void setTotalDeAvaliacoes(int totalDeAvaliacoes) {
-        this.totalDeAvaliacoes = totalDeAvaliacoes;
-    }
-
-    public double getSomaDasAvaliacoes() {
-        return somaDasAvaliacoes;
-    }
-
-    public void setSomaDasAvaliacoes(double somaDasAvaliacoes) {
-        this.somaDasAvaliacoes = somaDasAvaliacoes;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getAnoDeLancamento() {
-        return anoDeLancamento;
-    }
-
-    public void setAnoDeLancamento(int anoDeLancamento) {
-        this.anoDeLancamento = anoDeLancamento;
-    }
-
-    public boolean isIncluidoNoPlano() {
-        return incluidoNoPlano;
-    }
-
-    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
-        this.incluidoNoPlano = incluidoNoPlano;
-    }
-
-    public double getAvaliacao() {
-        return avaliacao;
-    }
-
-    public void setAvaliacao(double avaliacao) {
-        this.avaliacao = avaliacao;
-    }
-
-    public int getDuracaoEmMinutos() {
-        return duracaoEmMinutos;
-    }
-
-    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
-        this.duracaoEmMinutos = duracaoEmMinutos;
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
     }
 }
